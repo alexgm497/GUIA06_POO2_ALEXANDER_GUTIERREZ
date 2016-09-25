8c3bd1e64bee46ac0452aef9379baad294bf7b78
Alexander <Alexander@192.168.1.14>
2016-09-24 23:06:40 -0600

    CRUD Terminado :'D

--- GUIA06_POO2_ALEXANDER_GUTIERREZ/src/main/java/com/sv/udb/controlador/GruposBean.java
+++ GUIA06_POO2_ALEXANDER_GUTIERREZ/src/main/java/com/sv/udb/controlador/GruposBean.java
@@ -13,6 +13,7 @@ import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import org.primefaces.context.RequestContext;
 
 /**
  *
@@ -20,27 +21,111 @@ import javax.enterprise.context.RequestScoped;
  */
 @Named(value = "gruposBean")
 @RequestScoped
-public class GruposBean implements Serializable{
+public class GruposBean implements Serializable {
 
     @EJB
     private GruposFacadeLocal FCDEGrupos;
     private List<Grupos> grupos;
+    private Grupos objeGrupos;
+    private boolean guardar;
 
     /**
      * Creates a new instance of GruposBean
      */
     public GruposBean() {
     }
-    
-    @PostConstruct
-    public void init() {
-        this.grupos = FCDEGrupos.findAll();
-    }
+
     public List<Grupos> getGrupos() {
         return grupos;
     }
 
     public void setGrupos(List<Grupos> listGrup) {
         this.grupos = listGrup;
+    }
+
+    public Grupos getObjeGrupos() {
+        return objeGrupos;
+    }
+
+    public void setObjeGrupos(Grupos objeGrupos) {
+        this.objeGrupos = objeGrupos;
+    }
+
+    public boolean isGuardar() {
+        return guardar;
+    }
+
+    public void setGuardar(boolean guardar) {
+        this.guardar = guardar;
+    }
+
+    @PostConstruct
+    public void init() {
+        this.objeGrupos = new Grupos();
+        this.guardar = true;
+        this.consTodo();
+        this.limpForm();
+    }
+
+    public void limpForm() {
+        this.objeGrupos = new Grupos();
+        this.guardar = true;
+    }
+
+    public void consTodo() {
+        try {
+            this.grupos = FCDEGrupos.findAll();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void guar() {
+        RequestContext ctx = RequestContext.getCurrentInstance(); //Capturo el contexto de la página        
+        try {
+            FCDEGrupos.create(objeGrupos);
+            this.guardar = true;
+            this.grupos.add(objeGrupos);
+            this.limpForm();
+            ctx.execute("setMessage('MESS_SUCC','Mensaje', 'Datos guardados');");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            ctx.execute("setMessage('MESS_ERRO','Mensaje', 'Datos NO guardados');");
+        }
+    }
+
+    public void cons(int codiAlum) {
+        try {
+            this.objeGrupos = FCDEGrupos.find(codiAlum);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void elim(int codiAlum) {
+        RequestContext ctx = RequestContext.getCurrentInstance();
+        try {
+            this.objeGrupos = FCDEGrupos.find(codiAlum);
+            FCDEGrupos.remove(objeGrupos);
+            this.grupos.remove(objeGrupos);
+            this.limpForm();
+            ctx.execute("setMessage('MESS_SUCC','Mensaje', 'Datos eliminados');");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            ctx.execute("setMessage('MESS_SUCC','Mensaje', 'Datos NO eliminados');");
+        }
+    }
+
+    public void actu() {
+        RequestContext ctx = RequestContext.getCurrentInstance();
+        try {
+            FCDEGrupos.edit(objeGrupos);
+            this.grupos = FCDEGrupos.findAll();
+            this.limpForm();
+            ctx.execute("setMessage('MESS_SUCC','Mensaje', 'Datos actualizados');");
+        } catch (Exception ex) {
+            ctx.execute("setMessage('MESS_SUCC','Mensaje', 'Datos NO actualizados');");
+            ex.printStackTrace();
+        }
     }
 }
